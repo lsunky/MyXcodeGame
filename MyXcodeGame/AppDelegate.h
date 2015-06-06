eb00f869da5b9a826e55634064437ea3e60ae601
刘勋 <liuxun@liuxundembp.idfsoft.com>
2015-06-06 17:23:27 +0800

    Initial Commit

---
+++ MyXcodeGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyXcodeGame
+//
+//  Created by 刘勋 on 15/6/6.
+//  Copyright (c) 2015年 刘勋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyXcodeGame/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  MyXcodeGame
+//
+
+//  Copyright (c) 2015年 刘勋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
